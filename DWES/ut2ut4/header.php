9005bef71281fb13becd6fa7994ef819a7b40167
creepykai <raleydg@gmail.com>
2025-10-07 22:30:02 +0200

    07/10

--- DWES/ut2ut4/header.php
+++ DWES/ut2ut4/header.php
@@ -1,4 +1,21 @@
+<?php
+$title = "Header";
+?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel= "stylesheet" href="styles.css">
+<head>
+    <meta charset = "UTF-8">
+    <link rel= "stylesheet" href="styles.css">
+    <title>echo $title</title>
+</head>
+<body>
+    <header>
+        <h1>Juego DWES</h1>
+        <nav>
+            <a href="index.php">Inicio</a> 
+            <a href="perfil.php">Perfil</a> 
+            <a href="editar.php">Editar preguntas</a> 
+            <a href="jugar.php">Jugar</a> 
+            <a href="mejores.php">Mejores/Peores</a>
+        </nav>
+    </header>
