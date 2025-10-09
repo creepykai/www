ece6a2955f3256e7fbf51c720cd0545b38646e61
creepykai <raleydg@gmail.com>
2025-10-09 13:47:28 +0200

    09/10

--- DWES/T3/Actividad1/1.php
+++ DWES/T3/Actividad1/1.php
@@ -1,16 +1,28 @@
 <?php
-$nom = $_GET['nombre'] ?? '';
+$nom = $_GET['nombre'] ?? ''; /*Si se usa _REQUEST cambiando el metodo (que por defecto es get)*/
 $ape = $_GET['apellido'] ?? '';
 $dir = $_GET['direccion'] ?? '';
 $tlf = $_GET['telefono'] ?? '';
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Actividad 1</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>apellido</th>
+            </tr>
+            <tr>
+                <td><?=$nom?></td>
+                <td><?=$ape?></td>
+
+            </tr>
+        </table>
 
 
-        <tr>
-            <th>Nombre</th>
-        </tr>
-        <tr>
-            <td><?=$nom?></td>
-        </tr>
 
