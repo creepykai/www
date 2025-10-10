4df74b1b67d060109313457c83934ec43cd3479f
creepykai <raleydg@gmail.com>
2025-10-10 10:22:57 +0200

    10/10

---
+++ practica_UT1/tarea2.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Schema;
+use GraphQL\Server\StandardServer;
+use GraphQL\Type\Definition\InterfaceType;
+ 
+$peliculas = [
+    ['id' => 0, 'actores' => [$actor[0], $actor[1]]],
+    ['id' => 0, 'actores' => [$actor[0], $actor[1]]]];
+
