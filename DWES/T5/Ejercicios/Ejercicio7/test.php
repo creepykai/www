c53535836a77d066bb34941842519741c93bad21
creepykai <raleydg@gmail.com>
2025-12-26 13:40:00 +0100

    26/12

--- DWES/T5/Ejercicios/Ejercicio7/test.php
+++ DWES/T5/Ejercicios/Ejercicio7/test.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once "versionAaron.php";
+
 $e1 = new EmpleadoModificado();
 $e2 = new EmpleadoModificado("Pepe", 2000);
 $e3 = new EmpleadoModificado("Ana", "Becario");
@@ -7,5 +9,23 @@ $e4 = new EmpleadoModificado("Luis", "Gerente");
 $e5 = new EmpleadoModificado("Maria", "Fijo");
 
 $phtml = ''. $e1 . '<br>' . $e2 . '<br>' . $e3 . '<br>' . $e4 . '<br>' . $e5;
-echo $phtml;
+
+$e = new ReflectionClass("EmpleadoModificado");
+
+$phtml .= "<pre>" . print_r($e->getMethods(), true) . "</pre>"
+."<pre>" . print_r($e->getProperties(), true) . "</pre>";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $phtml; ?>
+</body>
+</html>
+
 
--- DWES/T5/Ejercicios/Ejercicio7/versionAaron.php
+++ DWES/T5/Ejercicios/Ejercicio7/versionAaron.php
@@ -27,20 +27,52 @@ class EmpleadoModificado extends Empleado {
         }
         parent::__construct($valor1, $valor2);
     }
+
+    public function __toString() {
+        $ret = "<p>";
+        $ret .= "Nombre: " . $this->nombre . "<br>";
+        $ret .= "Tipo: " . get_class($this) . "<br>";
+        $ret .= "Sueldo: " . $this->sueldo . "</p>";
+        return $ret;
+    }
 }
 
 
 class Becario extends Empleado {
-    
+    function __construct(mixed $valor1 = "", mixed $valor2 = "") {
+        parent::__construct($valor1, $valor2);
+        $this->sueldo = 0;
+    }
+
+    function __toString() {
+        return "Becario: " . $this->nombre . " - " . $this->sueldo;
+    }
+
 
 }
 
 
 class Fijo extends Empleado {
+    function __construct(mixed $valor1 = "", mixed $valor2 = "") {
+        parent::__construct($valor1, $valor2);
+        if($this->sueldo < 650) $this->sueldo = 650;
+        if($this->sueldo > 1200) $this->sueldo = 1200;
+    }
 
+    function __toString() {
+        return "Fijo: " . $this->nombre . " - " . $this->sueldo;
+    }
 }
 
 
 class Gerente extends Empleado {
+    function __construct(mixed $valor1 = "", mixed $valor2 = "") {
+        parent::__construct($valor1, $valor2);
+        if($this->sueldo < 1500) $this->sueldo = 1500;
+        if($this->sueldo > 2500) $this->sueldo = 2500;
+    }
 
+    function __toString() {
+        return "Gerente: " . $this->nombre . " - " . $this->sueldo;
+    }
 }
